b26c95ff345568d2c0ac71a43aa5c3fd6771cdd8
Alex Seme <semenciuc.alex@gmail.com>
2023-03-26 18:21:18 +0300

    Chapter 9 Advanced Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,11 @@ class User < ApplicationRecord
     validates :password, presence: true, length: { minimum: 6 }
 
     # Returns the hash digest of the given string
-    def user.digest(string)
+    def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
+                                                      BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
-    end
+      end
 
  # Returns a random token.
     def User.new_token
@@ -28,4 +28,14 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, User.digest(remember_token))
     end
 
+      # Returns true if the given token matches the digest.
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    # Forgets a user.
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
