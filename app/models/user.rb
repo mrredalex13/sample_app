13922bcf91701cf7de138ba3c6036990d1612e79
Alex Seme <semenciuc.alex@gmail.com>
2023-03-29 12:59:04 +0300

    Chapter 12 Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     
@@ -17,6 +17,10 @@ class User < ApplicationRecord
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
     end
 
     # Activates an account.
@@ -52,10 +56,20 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, nil)
     end
 
+    # Sets the password reset attributes.
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_columns( reset_digest: FILL_IN, reset_sent_at: FILL_IN)
+    end
+
+    # Sends password reset email.
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
     private 
       # Converts email to all lower-case.
     def downcase_email
-
         self.email = email.downcase
     end
 
